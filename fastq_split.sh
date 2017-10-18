f4a41a8031ee92f87eb46822ab217321fbca1888
christacaggiano <christacaggiano@gmail.com>
2017-10-18 12:19:05 -0700

    small changes to integrate better

--- fastq_split.sh
+++ fastq_split.sh
@@ -4,12 +4,13 @@
 
 
 
-export INPUT_DIR=$1 
-export OUTPUT_DIR=$2 
+INPUT_DIR=$1 
+OUTPUT_DIR=$2 
+LIB=$3
 
 echo "Splitting of fastq"	
 
-for file in $(ls $INPUT_DIR*fastq.gz); do	
+for file in $(ls $INPUT_DIR/$LIB*fastq.gz); do	
 		echo $file	
 		SPLIT_NAME=$( basename $file | sed 's/fastq.gz//')
 		zcat $file | split -l 72000000 -d - $OUTPUT_DIR/$LIB/$SPLIT_NAME
