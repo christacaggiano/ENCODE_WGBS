f91520ae02db23838b1c5f7b2ed6948fbb655df4
christacaggiano <christacaggiano@gmail.com>
2017-10-18 12:21:03 -0700

    bismark methylation extraction

--- mergeUnsorted_dedup_files_for_methExtraction.sh
+++ mergeUnsorted_dedup_files_for_methExtraction.sh
@@ -5,38 +5,33 @@
 ### any positional sorting to be done, so you would want to merge all the unsorted bam files, output of bismark alignment, in to one merged bam file for methylation call later on.
 ### Basically, you would be skipping the sorting and indexing step here, which could save considerable computation time too.	
 
-RUN_PATH=`pwd`
 GENOME_PATH="/ye/zaitlenlabstore/christacaggiano/hg38/"
 BISMARK_PATH="/ye/zaitlenlabstore/christacaggiano/Bismark"
 SAMTOOLS_PATH="/ye/netapp/jimmie.ye/tools/samtools-1.3"
 TRIMGALORE_PATH="/ye/zaitlenlabstore/christacaggiano/trim-galore/TrimGalore-0.4.3"
 BOWTIE_PATH="/ye/zaitlenlabstore/christacaggiano/bowtie2-2.3.3"
 
-
-OUTPUT_DIR = $1
+OUTPUT_DIR=$1
 BAM_DIR=$2
-$LIB=$3
+LIB=$3
 
-if [[ ! -d $OUTPUT_DIR/unsortedButMerged_ForBismark_file ]]; then
-	mkdir $OUTPUT_DIR/unsortedButMerged_ForBismark_file
-fi
+echo "$OUTPUT_DIR"
 
-BAM_COUNT=$( ls $BAM_DIR/bam_files/*.bam | wc -l)
-UNSORTED_BAM_DIR=$BAM_DIR/unsortedButMerged_ForBismark_file
+BAM_COUNT=$( ls $BAM_DIR/*.bam | wc -l)
 
 ### Merge the unsorted bam files:
 if [[ $BAM_COUNT > 1 ]]; then
-            BAM_LIST=$( ls $BAM_DIR/bam_files/*.bam | tr '\n' ' ' )
+            BAM_LIST=$( ls $BAM_DIR/*.bam | tr '\n' ' ' )
             $SAMTOOLS_PATH/samtools merge -nf $OUTPUT_DIR/unsortedButMerged_ForBismark_file/${LIB}_unsorted_merged.bam $BAM_LIST
 
     else if [[ $BAM_COUNT = 1 ]]; then
-            cp $BAM_DIR/bam_files/*.bam $OUTPUT_DIR/unsortedButMerged_ForBismark_file/${LIB}_unsorted_merged.bam
+            cp $BAM_DIR/*.bam $OUTPUT_DIR/unsortedButMerged_ForBismark_file/${LIB}_unsorted_merged.bam
             
 	fi      
-    fi     
+fi     
 
 ### Run the deduplication, and remove the pcr duplicates from unsorted_bam_files (i.e the sequence aligning to the same genomic positions).
-$BISMARK_PATH/deduplicate_bismark -p --bam $OUTPUT_DIR/unsortedButMerged_ForBismark_file/${LIB}_unsorted_merged.bam
+$BISMARK_PATH/deduplicate_bismark -p --bam $OUTPUT_DIR/unsortedButMerged_ForBismark_file/${LIB}_unsorted_merged.bam --output_dir $OUTPUT_DIR/unsortedButMerged_ForBismark_file
 
 BAM_PATH=$OUTPUT_DIR/unsortedButMerged_ForBismark_file
 
@@ -52,9 +47,8 @@ fi
 METH_OUTPUT_DIR=$OUTPUT_DIR/unsortedButMerged_ForBismark_file/methylation_extraction
 
 ### BAM files should be unsorted, and Default = CpG context only; else use --CX_context for all CpG info.
-for bam_file in $(/bin/ls $BAM_PATH/*_unsorted_merged.deduplicated.bam); do
+for bam_file in $(ls $BAM_PATH/*_unsorted_merged.deduplicated.bam); do
 	$BISMARK_PATH/bismark_methylation_extractor -p --no_overlap -o $METH_OUTPUT_DIR --comprehensive --report --bedGraph --genome_folder $GENOME_PATH $bam_file	
 	echo ""
 	done
-
 
