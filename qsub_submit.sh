d9a4a662ef3e6210bbe92146eba97175d5dba614
Christa.Caggiano <christacaggiano@gmail.com>
2018-09-06 10:29:35 -0700

    removed jensen files

--- qsub_submit.sh
+++ qsub_submit.sh
@@ -4,12 +4,15 @@
 #$ -cwd                            
 #$ -r y                            
 #$ -j y                           
-#$ -l mem_free=20G                 
+#$ -l mem_free=15G                 
 #$ -l arch=linux-x64               
 #$ -l netapp=2G,scratch=2G         
-#$ -l h_rt=24:00:00      
-#$ -t 1-114
+#$ -l h_rt=336:00:00
+#$ -t 9:10  
 
+input="/ye/zaitlenlabstore/christacaggiano/cfDNA_project/second_primary_methylation_data_ALS/fastq_files/zaitlenn-"$SGE_TASK_ID"_BC"
+output="/zaitlen/netapp/group/christa"
+fastq_prefix=$SGE_TASK_ID
+scripts="scripts"
 
-
-python wgbs_last_step.py  /ye/zaitlenlabstore/cfDNA/fastq /ye/zaitlenlabstore/christacaggiano/dna-me-pipeline/HAIB/bismark_pipeline $SGE_TASK_ID 
+python wgbs.py $input $output $fastq_prefix 
