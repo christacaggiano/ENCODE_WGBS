82d4d9a31f54bc6ea94dcbf2847692b28d3191b7
Christa.Caggiano <christacaggiano@gmail.com>
2018-09-06 10:46:16 -0700

    commented qsub file

--- qsub_submit.sh
+++ qsub_submit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash                         
 
+################ qsub parameters ###########################
 #$ -S /bin/bash                     
-#$ -cwd                            
+#$ -cwd                             
 #$ -r y                            
 #$ -j y                           
 #$ -l mem_free=15G                 
@@ -10,9 +11,19 @@
 #$ -l h_rt=336:00:00
 #$ -t 9:10  
 
+################## environment ###############################
+GENOME_PATH="/ye/zaitlenlabstore/christacaggiano/hg38/"
+BISMARK_PATH="/ye/zaitlenlabstore/christacaggiano/Bismark"
+SAMTOOLS_PATH="/ye/netapp/jimmie.ye/tools/samtools-1.3"
+TRIMGALORE_PATH="/ye/zaitlenlabstore/christacaggiano/trim-galore/TrimGalore-0.4.3"
+BOWTIE_PATH="/ye/zaitlenlabstore/christacaggiano/bowtie2-2.3.3"
+
+################ experiment directories ######################
 input="/ye/zaitlenlabstore/christacaggiano/cfDNA_project/second_primary_methylation_data_ALS/fastq_files/zaitlenn-"$SGE_TASK_ID"_BC"
 output="/zaitlen/netapp/group/christa"
 fastq_prefix=$SGE_TASK_ID
 scripts="scripts"
 
-python wgbs.py $input $output $fastq_prefix 
+
+# runs the pipeline 
+python wgbs.py $input $output $fastq_prefix $scripts
